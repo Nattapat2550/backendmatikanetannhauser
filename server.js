9ff032d07d925ff661af2e4f30f3fb06003b23f8
Nattapat2550 <nyansungvon@gmail.com>
2026-04-18 22:12:18 +0700

    edit option

--- server.js
+++ server.js
@@ -27,7 +27,7 @@ app.use(cors({
     origin: process.env.CLIENT_URL || 'http://localhost:3000',
     credentials: true,
 }));
-app.options('*', cors());
+app.options(/.*/, cors());
 
 app.use(express.json());
 app.use(cookieParser());
