fc28990b85bf1a55c6c02edac1471ef6924744eb
opsifiz <fifafayya@gmail.com>
2026-04-24 21:13:15 +0700

    install swagger and swagger's config

--- server.js
+++ server.js
@@ -65,7 +65,7 @@ const swaggerOptions = {
         },
         servers: [
             {
-                url: 'http://localhost:5000/api/v1',
+                url: 'http://localhost:3000/api/v1',
             }
         ],
     },
